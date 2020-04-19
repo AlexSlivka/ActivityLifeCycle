e991d2a345f87060723700be9cf33ab07df36d10
Aleksei Slivka <alexprospero25@gmail.com>
2020-04-19 18:13:51 +0300

    Activity жизненый цикл, с выводом данных в консоль.

---
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.activitylifecycle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LIFECYCLE = "LIFE_CYCLE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(savedInstanceState == null){
+            makeToast("Первый запуск - onCreate()");
+           } else {
+            makeToast("Повторный запуск - onCreate()");
+            }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        makeToast("onStart()");
+        }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        makeToast("Повторный запуск - onRestoreInstanceState()");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        makeToast("Сохранение данных - onSaveInstanceState()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        makeToast("onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        makeToast("onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        makeToast("onStop()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        makeToast("onRestart()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        makeToast("onDestroy()");
+    }
+
+    private void makeToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+        Log.d(LIFECYCLE,message);
+    }
+}
